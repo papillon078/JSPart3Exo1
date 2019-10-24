234b6aa543645de4bb34c8b6ab97ed3841a255da
papillon078 <ldc.morin@gmail.com>
2019-10-24 16:03:49 +0200

    exercice corrigé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
+//on utilise la méthode "getElementById" de l'objet "document" pour changer la proriéré CSS de la bordure.
 function borderOn(){
-  document.getElementById("image1").style.border = "3px solid red";
+  document.getElementById('image1').style.border = '3px solid red';
 }
 function borderOff(){
-  document.getElementById("image1").style.border = "";
+  document.getElementById('image1').style.border = '';
 }
